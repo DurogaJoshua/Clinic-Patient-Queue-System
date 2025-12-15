b9ec37c8ebbd2951a81a334cf16e6208bb6b9f90
Canezaresjoebert <canezaresjoebert1@gmail.com>
2025-12-15 20:30:23 +0800

    Add Patient Routes file by Canezaresjoebert

---
+++ routes/patientRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  getPatients,
+  createPatient,
+  updatePatient,
+  deletePatient
+} from "../controllers/patientsController.js";
+
+const router = express.Router();
+
+router.get("/", getPatients);
+router.post("/", createPatient);
+// pag add han delete route pero gamiton an query parameter para ha ID
+router.put("/:id", updatePatient);
+router.delete("/", deletePatient);
+
+export default router;
